73e98e1a3b15c346dbacc11a89000aeef176f5e4
Szekeres Zente <zenteszekeres@gmail.com>
2026-09-23 21:32:30 +0200

    .

--- crud.py
+++ crud.py
@@ -1,7 +1,5 @@
 import models
 from sqlalchemy.orm import Session
 
-# Korábban: load_json() és keresés listában
-# SQLAlchemy-vel:
 def get_user(db: Session, user_id: int):
     return db.query(models.UserModel).filter(models.UserModel.id == user_id).first()
